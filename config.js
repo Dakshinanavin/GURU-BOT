8e3ae06bb438280de1934e1dd19d0d2de9055a04
Dakshinanavin <dakshinaaththanayaka672@gmail.com>
2024-05-04 20:32:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 
-global.owner = [['94774280298', 'dasee', true], ['94774280298', 'naveen', true], ['']] //Number of owners
+global.owner = [['94763768964', 'dasee', true], ['94763768964', 'naveen', true], ['']] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
